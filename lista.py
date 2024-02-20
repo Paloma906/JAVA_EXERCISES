eeb4fcd87fb45e1d60b09e528816d2ac69d88ca7
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-02-20 16:36:38 -0300

    Comandos, vetor.

---
+++ lista.py
@@ -0,0 +1,43 @@
+lista=[22,32,44,56,12,22,78,56,11,75]
+
+#print(lista[4],lista[6])
+
+#lista.append(55)
+#lista.insert(1,55) // o comando insert insere em uma posição
+#print(len(lista))
+#lista.pop()  // se não informar o andar, ele apaga o último andar (remove a posição)
+#lista.pop(4)
+#lista.remove(22)
+
+#print(max(lista))
+#print(min(lista))
+#print(sum(lista))
+#print('-------')
+#print(lista)
+
+
+#lista.sort()
+#print('------------')
+#print(lista)
+
+
+#lista.reverse()
+#print('----------')
+#print(lista)
+
+
+#lista.sort() // o sort só funciona em ordem crescente, para fazer em ordem descrecente. Primeiro faz o sort e depois o reverse
+#lista.reverse()
+#print(lista)
+
+
+
+#append() - adicionar um valor a última posição
+#len() - informa a quantidade de itens de um vetor
+#pop - retira um elemento do vetor
+#remove() - retira um elemento do vetor
+#max() - informa o maior valor do vetor
+#min() - informa o menor valor do vetor
+#sum() - calcula o somatório do vetor
+#sort() - ordena os valores do vetor
+#reverse() - reverte a ordem do vetor
